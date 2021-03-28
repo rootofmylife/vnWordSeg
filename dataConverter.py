7972890ceee4bd6d7b77ec96af0af4bad10fcbb8
rootofmylife <dotrananhduc@gmail.com>
2021-03-28 23:47:00 +0700

    Update dataConverter.py
    - Create FTS table

--- dataConverter.py
+++ dataConverter.py
@@ -1,10 +1,10 @@
 import sqlite3
 import pandas as pd
 
-conn = sqlite3.connect('./my_data.db')
+conn = sqlite3.connect('./my_fts_data.db')
 c = conn.cursor()
 
-c.execute('''CREATE TABLE dict (word text, POS text, definition text)''')
+c.execute('''CREATE VIRTUAL TABLE dict USING FTS5(word, POS, definition)''')
 
 users = pd.read_csv('/Users/ducdo/Downloads/full_dic.tsv', sep='\t')
 
